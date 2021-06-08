5d54e6351078bdbb344862b5114dc989ffb9e66b
Nathalia Novais <nathalianovais2004@gmail.com>
2021-06-08 11:15:02 -0300

    Marca

--- interfaces/IMarca.cs
+++ interfaces/IMarca.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+using Projeto_produtos.classes;
+
 namespace Projeto_produtos.interfaces
 {
     public interface IMarca
     {
-         
+        string Cadastrar(Marca marca);
+        List<Marca> Listar();
+        string Deletar(Marca marca);
     }
 }
