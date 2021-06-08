b88bdf758a35c51fad1f603a72ff8b3aaec20981
Nathalia Novais <nathalianovais2004@gmail.com>
2021-06-08 16:33:54 -0300

    brench marca realizada

--- classes/Marca.cs
+++ classes/Marca.cs
@@ -1,9 +1,45 @@
+using System;
+using System.Collections.Generic;
+using Projeto_produtos.interfaces;
+
 namespace Projeto_produtos.classes
 {
-    public class Marca
+    public class Marca : IMarca
     {
-       private int codigo { get; set; }
-       
-        
+
+
+        private int Codigo { get; set; }
+        private string NomeMarca { get; set; }
+        private DateTime DataCadastro { get; set; }
+        List<Marca> ListaMarca = new List<Marca>();
+
+        public Marca()
+        {
+
+        }
+        public Marca(int _codigo, string _nomemarca)
+        {
+            this.NomeMarca = _nomemarca;
+            this.Codigo = _codigo;
+            DataCadastro = DateTime.Now;
+        }
+
+
+        public string Cadastrar(Marca marca)
+        {
+            ListaMarca.Add(marca);
+            return "Marca cadastrada";
+        }
+
+        public string Deletar(Marca marca)
+        {
+             Console.WriteLine($"Qual o nome da marca que você deseja remover?");
+             string MarcaDeletada= Console.ReadLine();
+
+            ListaMarca.RemoveAll(x => x.NomeMarca == MarcaDeletada);
+            return "Deletado";
+        }
+
+
     }
 }
--- interfaces/IMarca.cs
+++ interfaces/IMarca.cs
@@ -6,7 +6,6 @@ namespace Projeto_produtos.interfaces
     public interface IMarca
     {
         string Cadastrar(Marca marca);
-        List <Marca> Listar();
         string Deletar(Marca marca);
     }
 }
